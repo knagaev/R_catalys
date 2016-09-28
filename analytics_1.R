99a248a254b48cdcb84dabca49ef122b225c7f31
knagaev <knagaev@gmail.com>
2016-09-28 10:19:03 +0300

    1st

--- analytics_1.R
+++ analytics_1.R
@@ -3,14 +3,13 @@ library("readxl")
 col_types <- readxl:::xlsx_col_types("../catalys/merged_master_families_matrix_ver_0_13.xlsx")
 mmfm <- read_excel("../catalys/merged_master_families_matrix_ver_0_13.xlsx", sheet = "merged_master_families_matrix", col_types=col_types)
 
-as.character(as.POSIXct(as.numeric(mmfm[["Priority date"]]) * (60*60*24)
-           , origin="1899-12-30"
-           , tz="GMT"), format = "%Y%m%d")
+#as.character(as.POSIXct(as.numeric(mmfm[["Priority date"]]) * (60*60*24), origin="1899-12-30", tz="GMT"), format = "%Y%m%d")
 
-mmfm[nchar(mmfm[["Priority date"]]) == 5, "Priority date"]
+#mmfm[nchar(mmfm[["Priority date"]]) == 5, "Priority date"]
 
-mmfm["Priority date"] <- as.character(as.POSIXct(as.numeric(mmfm[nchar(mmfm[["Priority date"]]) == 5, "Priority date"]) * (60*60*24)
-                        , origin="1899-12-30"
-                        , tz="GMT"), format = "%Y%m%d")
+mmfm[nchar(mmfm[["Priority date"]]) == 5, "Priority date"] <- 
+  as.character(as.POSIXct(as.numeric(mmfm[nchar(mmfm[["Priority date"]]) == 5, "Priority date"]) * (60*60*24)
+    , origin="1899-12-30"
+    , tz="GMT"), format = "%Y%m%d")
 
 
