2f3cc6a2da9837e6cb399bb2ec6ba0b9e618f0a8
Konstantin Nagaev <knagaev@gmail.com>
2016-10-16 02:40:17 +0300

    LDA

--- xml.R
+++ xml.R
@@ -1,6 +1,7 @@
 library(XML)
-z <- unz("Patent Document 0001488729.zip", filename = 'PatentDocument.xml', 'r') 
-doc <- xmlParse(file = z)
+z <- unz("Patent Document 0001488729.zip", filename = "PatentDocument.xml", 'r') 
+# doc <- xmlParse(file = z)
+doc <- xmlParse("PatentDocument.xml")
 df <- xmlToDataFrame(
   getNodeSet(doc, "//claims[@lang='en']"),
   colClasses=c("character"),
