60ab74dca7048e192a4a43054beb2939138a4008
Konstantin Nagaev <knagaev@gmail.com>
2016-10-17 17:35:20 +0300

    LDA

---
+++ lda_helper.R
@@ -0,0 +1,57 @@
+library(xml2)
+
+get_contents <- function(doc, tag_name, lang = "en")
+{
+  paste(
+    xml_text(
+      xml_find_all(doc, paste0("//", tag_name, "[@lang='", lang, "']//text()", sep = ""))
+    ), 
+    collapse = " ")
+}
+
+patent_path <- "D:/Downloads/Рефераты и формулы/Рефераты и формулы/"
+
+patent_files <- list.files(patent_path)
+# n_files <- length(patent_files)
+n_files <- 20 # пока ограничил 20 файлами
+
+v_num <- numeric(n_files)
+v_filename <- character(n_files)
+
+v_description_en <- character(n_files)
+v_claims_en <- character(n_files)
+v_abstract_en <- character(n_files)
+
+v_description_ru <- character(n_files)
+v_claims_ru <- character(n_files)
+v_abstract_ru <- character(n_files)
+
+for(i in 1:length(patent_files))
+{
+  print(patent_files[i])
+  
+  zip_name <- paste0(patent_path, patent_files[i])
+  doc <- read_xml(unz(zip_name, filename = "PatentDocument.xml", open = "rb")) 
+  
+  v_num[i] <- i
+  v_filename[i] <- patent_files[i]
+  
+  v_description_en[i] <- get_contents(doc, "description", "en")
+  v_claims_en[i] <- get_contents(doc, "claims", "en")
+  v_abstract_en[i] <- get_contents(doc, "abstract", "en")
+  
+  v_description_ru[i] <- get_contents(doc, "description", "ru")
+  v_claims_ru[i] <- get_contents(doc, "claims", "ru")
+  v_abstract_ru[i] <- get_contents(doc, "abstract", "ru")
+}
+
+df_patents <- data.frame(v_num, v_filename, 
+                         v_description_en, v_claims_en, v_abstract_en, 
+                         v_description_ru, v_claims_ru, v_abstract_ru, 
+                         stringsAsFactors=FALSE)
+
+write.table(df_patents, "df_patents.csv", fileEncoding = "UTF-8")
+
+
+# titleEnhanced
+# abstractEnhanced
