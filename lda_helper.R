814da628dc783ae8d0aeae7e328219fbd7f91df6
Konstantin Nagaev <knagaev@gmail.com>
2016-10-19 09:48:36 +0300

    LDA

--- lda_helper.R
+++ lda_helper.R
@@ -1,4 +1,8 @@
 library(xml2)
+library(readr)
+library(tibble)
+library(dplyr)
+library(purrr)
 
 get_contents <- function(doc, tag_name, lang = "en")
 {
@@ -9,11 +13,11 @@ get_contents <- function(doc, tag_name, lang = "en")
     collapse = " ")
 }
 
-patent_path <- "D:/Downloads/Рефераты и формулы/Рефераты и формулы/"
+patent_path <- "./patents/"
 
-patent_files <- list.files(patent_path)
-# n_files <- length(patent_files)
-n_files <- 20 # пока ограничил 20 файлами
+patent_files <- dir(patent_path)
+n_files <- length(patent_files)
+# n_files <- 20 # пока ограничил 20 файлами
 
 v_num <- numeric(n_files)
 v_filename <- character(n_files)
@@ -26,12 +30,13 @@ v_description_ru <- character(n_files)
 v_claims_ru <- character(n_files)
 v_abstract_ru <- character(n_files)
 
-for(i in 1:length(patent_files))
+for(i in seq(n_files))
 {
   print(patent_files[i])
   
+  #zip_name <- "Patent Document 0000016118.zip"  
   zip_name <- paste0(patent_path, patent_files[i])
-  doc <- read_xml(unz(zip_name, filename = "PatentDocument.xml", open = "rb")) 
+  doc <- xml_ns_strip(read_xml(zip_name, filename = "PatentDocument.xml")) 
   
   v_num[i] <- i
   v_filename[i] <- patent_files[i]
@@ -51,7 +56,6 @@ df_patents <- data.frame(v_num, v_filename,
                          stringsAsFactors=FALSE)
 
 write.table(df_patents, "df_patents.csv", fileEncoding = "UTF-8")
-
 
 # titleEnhanced
 # abstractEnhanced
