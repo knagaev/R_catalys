a3825235b4b50996feae0b709c418472b21b816e
knagaev <knagaev@gmail.com>
2017-04-28 12:12:59 +0300

    purrr

---
+++ purrr.R
@@ -0,0 +1,12 @@
+library(xml2)
+library(purrr)
+
+main_path <- 'C:\\_in\\prod\\advprop\\advan\\'
+
+
+pvis <- dir(main_path) %>% 
+  paste0(main_path, .) %>% 
+  map(read_xml) %>% 
+  map(~xml_text(xml_find_all(.,"//advp:PropertyVarietyId/wsrls:Id"))) %>% 
+  unlist %>% 
+  table
