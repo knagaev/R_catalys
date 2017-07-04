6e44e2eaae2a6c46641bf1b3bad9b3ae5b73047c
knagaev <knagaev@gmail.com>
2017-07-04 13:12:07 +0300

    20170704

--- tidytext.R
+++ tidytext.R
@@ -5,6 +5,7 @@ library(forcats)
 library(tidytext)
 library(ggplot2)
 library(tm)
+library(slam)
 
 main_path <- "C:\\PLR\\catalys\\PsDocs\\PsDocs 2017-06-16"
 
@@ -32,16 +33,21 @@ psdocs <- dir(main_path, pattern = "*.xml", full.names = TRUE, recursive = TRUE)
   map(get_propvar) %>% 
   bind_rows
 
-df_corpus <- psdocs %>%
-  transmute(id = id, 
-            text_contents = paste(title, description, claims, abstract, sep = ' '))
+#df_corpus <- psdocs %>%
+#  transmute(id = id, 
+#            text_contents = paste(title, description, claims, abstract, sep = ' '))
 
+df_corpus <- psdocs %>%
+  unite(text_contents, title, description, claims, abstract, sep = " ")
 
 # заменяем пунктуацию на пробел
 df_corpus$text_contents <- gsub("[[:punct:]]", " ", df_corpus$text_contents)
+df_corpus$text_contents <- gsub("[“”\"]", " ", df_corpus$text_contents)
+df_corpus$text_contents <- gsub("[^а-яА-Я]", " ", df_corpus$text_contents)
 
 #df_corpus <- column_to_rownames(df_corpus, var = "id")
 
+# сохраняем id документа в поле id
 myReader <- readTabular(mapping=list(content="text_contents", id="id"))
 wordCorpus <- Corpus(DataframeSource(df_corpus), readerControl=list(reader=myReader))
 
@@ -49,9 +55,19 @@ wordCorpus <- Corpus(DataframeSource(df_corpus), readerControl=list(reader=myRea
 wordCorpus <- tm_map(wordCorpus, removeNumbers)
 wordCorpus <- tm_map(wordCorpus, content_transformer(tolower))
 wordCorpus <- tm_map(wordCorpus, removeWords, stopwords("russian"))
+wordCorpus <- tm_map(wordCorpus, removeWords, stopwords("english"))
+
+local_stopwords <- c("фиг", "патент", "дата", "публикации", "осуществляется", 
+                     "путем", "средства", "является", "который", "содержит", 
+                     "относится", "области", "область", "включает", "например", 
+                     "имеющий",  "служит", "также", "авт",  "свид", "датчиков", 
+                     "средств")
+wordCorpus <- tm_map(wordCorpus, removeWords, local_stopwords)
+wordCorpus <- tm_map(wordCorpus, stripWhitespace)
 #wordCorpus <- tm_map(wordCorpus, content_transformer(stemDocument), "russian")
 
-dtm <- DocumentTermMatrix(wordCorpus, control = list(stemming = FALSE, stopwords = FALSE, minWordLength = 3,
+dtm <- DocumentTermMatrix(wordCorpus, control = list(stemming = FALSE, stopwords = FALSE, wordLengths=c(3, 20),
+                                                     bounds = list(global = c(2, Inf)),
                                                      removeNumbers = FALSE, removePunctuation = FALSE))
 dim(dtm)
 
@@ -66,7 +82,7 @@ term_tfidf <- tapply(lda_dtm$v/row_sums(lda_dtm)[lda_dtm$i], lda_dtm$j, mean) * 
 dim(term_tfidf)
 summary(term_tfidf)
 
-lda_dtm <- lda_dtm[, term_tfidf >= 0.01 & term_tfidf < 0.1]
+lda_dtm <- lda_dtm[, term_tfidf >= 0.005 & term_tfidf < 0.1]
 lda_dtm <- lda_dtm[row_sums(lda_dtm) > 0, ]
 summary(col_sums(lda_dtm))
 dim(lda_dtm)
@@ -87,26 +103,30 @@ dtmr$dimnames[["Terms"]][which(term_tfidf > 0.1)]
 
 ap_lda <- LDA(lda_dtm, k = 5, control = list(seed = 1234))
 
-ap_lda <- jss_TM[["VEM_fixed"]]
+#ap_lda <- jss_TM[["VEM_fixed"]]
 
 ap_topics <- tidy(ap_lda, matrix = "beta")
 ap_topics
 
+df1 <- df_corpus %>% 
+  filter(str_detect(text_contents, "изг"))
+write_excel_csv(df1, "df1.csv")
+
 ap_top_terms <- ap_topics %>%
-#  mutate(term = factor(term), 
-#         n = as.numeric(factor(topic))) %>% 
-#  mutate(rep_term = paste(c(rep(' ', n - 1), term), collapse=''),
-#         sort_term = factor(rep_term, levels=ap_top_terms[order(beta), 'rep_term'])
-#         ) %>% 
+  unite(sort_term, topic, term, sep = " ", remove = FALSE) %>% 
+#  mutate(sort_term = paste0(topic, term, collapse='')) %>% 
   group_by(topic) %>%
   top_n(10, beta) %>%
   ungroup() %>%
   arrange(topic, -beta)
 
+
 ap_top_terms %>%
-  mutate(term = fct_reorder2(term, topic, beta, .desc = FALSE)) %>%
-  ggplot(aes(term, beta, fill = factor(topic))) +
-  geom_col(show.legend = FALSE) +
+  mutate(sort_term = fct_reorder2(sort_term, topic, beta, function(x, y) {-x * 10000 + y}, .desc = FALSE)) %>%
+  ggplot(aes(x = sort_term, beta, fill = factor(topic))) +
+# geom_bar(stat = "identity", show.legend = TRUE) +
+  geom_col(show.legend = TRUE) +
+  scale_x_discrete(labels = function(x) str_sub(x, 3)) +
   facet_wrap(~ topic, scales = "free") +
   coord_flip()
 
@@ -120,10 +140,14 @@ beta_spread
 
 ap_documents <- tidy(ap_lda, matrix = "gamma")
 ap_documents
+write_excel_csv(ap_documents, "ap_documents.csv")
+
 
 max_topics <- ap_documents %>% 
   group_by(document) %>% 
   filter(gamma == max(gamma))
+
+write_excel_csv(max_topics, "max_topics.csv")
 
 tidy(lda_dtm) %>%
   filter(document == "0002529781_C2_20140927_RU") %>%
@@ -146,3 +170,7 @@ jss_TM <-
        CTM = CTM(lda_dtm, k = k,
                  control = list(seed = SEED, var = list(tol = 10^-4), em = list(tol = 10^-3))))
 
+data.frame(dtm$dimnames["Terms"]) %>% 
+  arrange(str_length(Terms)) %>% 
+  mutate(l = str_length(Terms)) %>% 
+  head
